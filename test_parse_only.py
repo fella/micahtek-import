1a79e7cedc5d0a662d79642815f11dfeaf19a056
Dux McQuacken <quicksandee@gmail.com>
2026-04-14 12:07:05 -0700

    hope this fixes IT!

--- test_parse_only.py
+++ test_parse_only.py
@@ -1,6 +1,6 @@
 from src.parse_crd import parse_crd_file
 
-file_path = "fixtures/01015642.crd"   # change this to your real file
+file_path = "./fixtures/01015642.crd"   # change this to your real file
 
 records = parse_crd_file(file_path)
 print(f"Loaded {len(records)} records")
